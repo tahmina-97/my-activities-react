9ffb3cdbbb47103c87bfbf418c0a3a365492efcd
Tahmina Ria <tahminariya459@gmail.com>
2022-09-29 21:36:45 +0600

    style correction

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 
 import './App.css';
 import Activity from './components/Activity/Activity';
-import Question1 from './components/Question1/Question1';
-import Question2 from './components/Question2/Question2';
 import Questions from './components/Questions/Questions';
 
 function App() {
